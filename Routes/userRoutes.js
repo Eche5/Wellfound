dbb9a5c9375bbf7cb209b42b6daae64788386257
Eche5 <111073744+Eche5@users.noreply.github.com>
2023-11-21 19:55:34 +0100

    nodemon

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 const messageController = require("../Controller/messageController");
 const userController = require("../Controller/userController");
+const loginLimiter = require("../middlewares/LoginLimit");
 router.route("/register").post(userController.createUser);
-router.route("/auth").post(userController.Login);
+router.route("/auth").post(loginLimiter, userController.Login);
 router.route("/message/:id").get(messageController.getMessages);
+router.route("/forgotpassword").post(userController.forgotPassword);
+router.route("/verify").post(userController.resendverification);
+
+router.route("/verify/:id").patch(userController.verify);
+
+router.route("/resetpassword/:id/:token").patch(userController.resetPassword);
+
+router.route("/googleauth").get(userController.getOneUser);
 router.route("/messages/:id").get(messageController.getAllMessages);
 router.route("/users").get(userController.getAllUsers);
 
